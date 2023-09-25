8423ff4ee1b9dc6559c95106c8bcbd1e99b6378f
santi <santiagoc333@gmail.com>
2023-09-24 20:15:11 -0600

    implemented methods in ScoreTrakker class, need to figure out how to sort arraylist

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class ScoreTrakker {
@@ -5,15 +7,25 @@ public class ScoreTrakker {
 	
 	
 	public void loadDataFile(String filename) {
-		
+		try {
+			int line = 1;
+			FileReader reader = new FileReader(filename);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void printInOrder() {
-		
+		for(Student s : students) {
+			System.out.println(s);
+		}
 	}
 	
 	public void processFiles() {
-		
+		loadDataFile("scores.txt");
+		printInOrder();
 	}
 	public static void main(String[] args) {
 		
